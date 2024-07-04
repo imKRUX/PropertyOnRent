57958d0ddcd829b65d38d4304eeaed95167c5a3b
imkrux <wakeup.krupal@gmail.com>
2024-07-04 20:55:59 +0530

    1st commit

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+     title: "Property Plus | Find The Perfect Rentals",
+     discriptions: "Find Your Dream Rental Properties",
+};
+
+const MainLayout = () => {
+     return (
+          <html>
+               <body>
+                    <div>MainLayout</div>
+               </body>
+          </html>
+     );
+};
+
+export default MainLayout;
