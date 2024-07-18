7006ea1b3b6eff066ff1852cfbf80b331fb58473
imkrux <wakeup.krupal@gmail.com>
2024-07-18 20:49:07 +0530

    v

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
+import properties from "@/properties.json";
 
 const PropertiesPage = () => {
      return (
-          <div>
-               <h1 className="text-3xl"> Properties</h1>
-               <Link href="/">Go Home</Link>
-          </div>
+          <section class="px-4 py-6">
+               <div class="container-xl lg:container m-auto px-4 py-6">
+                    <div class="grid grid-cols-1 md:grid-cols-3 gap-6"></div>
+               </div>
+          </section>
      );
 };
 
