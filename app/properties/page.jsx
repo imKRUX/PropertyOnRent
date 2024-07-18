2abdd518a24726e092efa88e6eede83cdd991ec7
imkrux <wakeup.krupal@gmail.com>
2024-07-19 00:44:01 +0530

    adding PropertyCards

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,14 +1,22 @@
+import PropertyCard from "@/components/PropertyCard";
 import properties from "@/properties.json";
 
 const PropertiesPage = () => {
      return (
-          <section class="px-4 py-6">
-               <div class="container-xl lg:container m-auto px-4 py-6">
-                    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                         {properties.map((property) => (
-                              <div>{property.name}</div>
-                         ))}
-                    </div>
+          <section className="px-4 py-6">
+               <div className="px-4 py-6 m-auto container-xl lg:container">
+                    {properties.length === 0 ? (
+                         <p>No Properties Found !!!</p>
+                    ) : (
+                         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+                              {properties.map((property) => (
+                                   <PropertyCard
+                                        key={property._id}
+                                        property={property}
+                                   />
+                              ))}
+                         </div>
+                    )}
                </div>
           </section>
      );
